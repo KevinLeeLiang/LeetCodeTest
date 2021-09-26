f5503d27d660c6e6d6bf6745299f8e7c2b5d9298
lijialiang <lijialiang362>
2021-09-26 20:35:23 +0800

    提交第七题

--- leetcode_test/main.cpp
+++ leetcode_test/main.cpp
@@ -119,11 +119,38 @@ void MedianOfTwoSortedArrays() {
 	std::cout << "result:" << result << std::endl;
 }
 
+/*
+7.Reverse Interger
+题⽬
+Given a 32-bit signed integer, reverse digits of an integer.
+Example 1:
+	Input: 123
+	Output: 321
+
+Example 2:
+	Input: -123
+	Output: -321
+
+Example 3:
+	Input: 120
+	Output: 21
+
+Note: Assume we are dealing with an environment which could only store integers within the 32-
+bit signed integer range: [−2^31, 2^31 − 1]. For the purpose of this problem, assume that your
+function returns 0 when the reversed integer overflows.
+*/
+#include"ReverseInterger/ReverseInterger.h"
+void ReverseIntergerTest() {
+	long long test = 520;
+	std::cout << ReverseInterger(test) << std::endl;
+}
+
 int main() {
 	std::cout << "Hello World!" << std::endl;
 	//TwoSumTest();
 	//AddTwoNumbersTest();
 	//LongestSubstringWithoutRepeatingCharactersTest();
-	MedianOfTwoSortedArrays();
+	//MedianOfTwoSortedArrays();
+	ReverseIntergerTest();
 	return 0;			
 }
