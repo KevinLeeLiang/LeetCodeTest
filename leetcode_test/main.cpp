0f8540be98854b40b2e65f4e09bcf7914c611011
lijialiang <lijialiang362>
2021-09-29 10:38:33 +0800

    增加3sum的README.md和第16题3SumClosest代码

--- leetcode_test/main.cpp
+++ leetcode_test/main.cpp
@@ -240,6 +240,48 @@ void ThreeSumTest() {
 	}
 }
 
+/*
+16.3Sum Closest
+题⽬
+Given an array nums of n integers and an integer target, find three integers in nums such that the
+sum is closest to target. Return the sum of the three integers. You may assume that each input
+would have exactly one solution.
+Example:
+Given array nums = [-1, 2, 1, -4], and target = 1.
+The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+题⽬⼤意
+给定⼀个数组，要求在这个数组中找出 3 个数之和离 target 最近。
+解题思路
+这⼀题看似和第 15 题和第 18 题很像，都是求 3 或者 4 个数之和的问题，但是这⼀题的做法和 15，18
+题完全不同。
+这⼀题的解法是⽤两个指针夹逼的⽅法。先对数组进⾏排序，i 从头开始往后⾯扫。这⾥同样需要注意
+数组中存在多个重复数字的问题。具体处理⽅法很多，可以⽤ map 计数去重。这⾥笔者简单的处理，i
+在循环的时候和前⼀个数进⾏⽐较，如果相等，i 继续往后移，直到移到下⼀个和前⼀个数字不同的位
+置。j，k 两个指针开始⼀前⼀后夹逼。j 为 i 的下⼀个数字，k 为数组最后⼀个数字，由于经过排序，所
+以 k 的数字最⼤。j 往后移动，k 往前移动，逐渐夹逼出最接近 target 的值。
+*/
+#include"3SumClosest/3SumClosest.h"
+void ThreeSumClosestTest() {
+	std::vector<int>test = { -1, 2, 1, -4 };
+	int target = 1;
+	std::vector<int>res = ThreeSumClosest(test, target);
+	if (res.size() == 0) {
+		std::cout << "No Solution" << std::endl;
+		return ;
+	}
+	else {
+		std::cout << "[";
+		for (int i = 0; i < res.size(); i++) {
+			if (i == res.size() - 1) {
+				std::cout << res.at(i);
+			}
+			else
+				std::cout << res.at(i) << ",";
+		}
+		std::cout << "]" << std::endl;	
+	}
+}
+
 int main() {
 	std::cout << "Hello World!" << std::endl;
 	//TwoSumTest();
@@ -250,6 +292,7 @@ int main() {
 	//PalindromeNumberTest();
 	//ContainerWithMostWaterTest();
 	//RomanToIntegerTest();
-	ThreeSumTest();
+	//ThreeSumTest();
+	ThreeSumClosestTest();
 	return 0;			
 }
