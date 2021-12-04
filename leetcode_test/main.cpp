3fc3685f28c9feebee2ff474431091a91f637924
lijialiang <lijialiang362>
2021-12-04 18:29:08 +0800

    更新第37题的完整test注释

--- leetcode_test/main.cpp
+++ leetcode_test/main.cpp
@@ -1153,6 +1153,21 @@ A sudoku solution must satisfy all of the following rules:
 3. Each of the the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the
 grid.
 Empty cells are indicated by the character '.' .
+Note:
+The given board contain only digits 1-9 and the character '.' .
+You may assume that the given Sudoku puzzle will have a single unique solution.
+The given board size is always 9x9 .
+题⽬⼤意
+编写⼀个程序，通过已填充的空格来解决数独问题。⼀个数独的解法需遵循如下规则：
+1. 数字 1-9 在每⼀⾏只能出现⼀次。
+2. 数字 1-9 在每⼀列只能出现⼀次。
+3. 数字 1-9 在每⼀个以粗实线分隔的 3x3 宫内只能出现⼀次。
+空⽩格⽤ '.' 表示。
+解题思路
+给出⼀个数独谜题，要求解出这个数独
+解题思路 DFS 暴⼒回溯枚举。数独要求每横⾏，每竖⾏，每九宫格内， 1-9 的数字不能重复，每
+次放下⼀个数字的时候，在这 3 个地⽅都需要判断⼀次。
+另外找到⼀组解以后就不需要再继续回溯了，直接返回即可。
 */
 #include "SudokuSolver/sudoku_solver.h"
 void SudokuSolverTest() {	 
